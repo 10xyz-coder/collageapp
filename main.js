2e9038a0f906167956a4f6b89837db80fbc22ea2
10xyz <82887902+10xyz-coder@users.noreply.github.com>
2022-12-17 16:53:08 +0530

    17-12-22 Update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const SpeechRecog = window.webkitSpeechRecognition;
 var recog = new SpeechRecog();
 const textbox = document.getElementById("textbox")
 const camera = document.getElementById("camera")
-const snaps = ["take my selfie","take a photo","snap","click a picture","secret word"]
+const snaps = ["take my collage","take a photo","snap","click a picture","secret word"]
 
 function start() {
   textbox.innerText = "";
@@ -15,7 +15,7 @@ function sleep(ms) {
 }
 
 function end(confidence) {
-  if (confidence < 0.75) {
+  if (confidence < 0.25) {
     console.log('Speech recognition has stopped due to low confidence.');
     recog.abort();
     alert("Please Try Again")
@@ -27,7 +27,7 @@ recog.onresult = function(event) {
 
   var Content = event.results[0][0].transcript;
   var Confidence = event.results[0][0].confidence;
-  if (Confidence >= 0.75) {
+  if (Confidence >= 0.25) {
     console.log(Content);
     console.log(Confidence);
     textbox.innerText = Content;
@@ -43,24 +43,28 @@ async function speak() {
   var synth = window.speechSynthesis;
   speak_data = textbox.value;
 
-  var utterThis = new SpeechSynthesisUtterance("Taking a selfie! in 5.");
+  var utterThis = new SpeechSynthesisUtterance("Taking a collage! in 5.");
   synth.speak(utterThis);
   Webcam.attach(camera)
-  await sleep(5000);
-  takeSnap()
+  for (let i = 0; i < document.getElementById('times').value; i++) {
+    await sleep(3000);
+    takeSnap(i);
+  }
+  Webcam.reset()
 }
 
 Webcam.set({width:360,height:250,image_format:"png",png_quality:90})
 
-function takeSnap() {
+function takeSnap(imgo) {
   Webcam.snap( function(data_uri) {
-		document.getElementById('result').innerHTML = '<img id="result-img" src="'+data_uri+'"/>';
+		document.getElementById('result').innerHTML += '<img id="result-img-'+ imgo.toString() +'" src="'+data_uri+'"/>';
 	} );
-  Webcam.reset()
 }
 
 function saveSnap() {
   var link = document.getElementById("link");
-  link.href = document.getElementById('result-img').src;
-  link.click();
+  for (let i = 0; i < document.getElementById('times').value; i++) {
+    link.href = document.getElementById('result-img-'+i.toString()).src;
+    link.click();
+  }
 }
